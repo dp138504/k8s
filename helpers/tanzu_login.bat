19df96619ff34b877131145c3c7b00bb5a4fbf3c
Dylan Pitts <pitts.dylan@gmail.com>
2022-03-28 18:06:55 -0400

    My ESXi host died so I had to redeploy.

--- helpers/tanzu_login.bat
+++ helpers/tanzu_login.bat
@@ -1,1 +1,1 @@
-kubectl vsphere login --insecure-skip-tls-verify --server 172.16.4.13 --tanzu-kubernetes-cluster-name crossfits-tkc
+kubectl vsphere login --insecure-skip-tls-verify --server 172.16.4.10 --tanzu-kubernetes-cluster-name crossfits-tkc
